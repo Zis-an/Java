6af3d3eaedb1f113d4eafd26dcd0efa94c89f67b
nurnobitechyfo <nurnobi@techyfo.com>
2024-12-01 18:43:49 +0600

    first commit

---
+++ Data_Types_4/Hello.java
@@ -0,0 +1,16 @@
+class Hello {
+    public static void main(String a[])
+    {
+        int num1 = 9;
+        byte num2 = 127;
+        short num3 = 558;
+        long num4 = 5854l;
+
+        float num5 = 5.8f;
+        double num6 = 5.8;
+
+        boolean b = true;
+
+        System.out.println( num1 + ", " + num2 + ", " + num3 + ", " + num4 + ", " + num5 + ", " + num6 + ", " + b);
+    }
+}
---
+++ Hello_World_1/Hello.java
@@ -0,0 +1,11 @@
+// Java Code is turned into Byte Code using Compiler (Java Compiler). 
+// Then JVM runs the byte Code 
+
+
+class Hello
+{
+    public static void main(String a[])
+    {
+        System.out.print("Hello World");
+    }
+}
